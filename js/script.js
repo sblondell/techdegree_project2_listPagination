09a31dc41e8582dea8b6d0758de48810e99ff5d3
Stephan Blondell <stephan.blondell@gmail.com>
2018-12-15 17:41:06 -0500

    added listener event to dynamically appended navigation links

--- js/script.js
+++ js/script.js
@@ -3,30 +3,10 @@
 /*                                                       */
 /* Using HTML5, CSS, and Javascript to create a webpage  */
 /* that divides one list into digestible chunks.         */
-/* #Extra Credit */
-   
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+
 const studentList = document.querySelector('.student-list').children;
 const entriesPerPage = 10;
 const numberOfPages = Math.ceil(studentList.length / entriesPerPage);
-
-/*const hideList = function(){
-  let i = 0;
-
-  while (studentList[i]){
-    studentList[i].style.display = 'none';
-    i++;
-  }
-}*/
 
 const manipulateEl = {
   create : (element, property, value) => {
@@ -57,21 +37,6 @@ const showPage = (list, page) => {
   //Clear the list from the page
   hideList();
 
-  /*
-  if (page >= numberOfPages){ //Unique Conditional: if the last page is selected, display the last page
-    let i = pageFloor;
-    while (studentList[i]){
-      studentList[i].style.display = '';
-      i++;
-    }
-  }else{
-    for (let i = pageFloor; i < pageCeiling; i++){
-      studentList[i].style.display = '';
-    }
-  }
-  */
-
-  //let i = pageFloor;
   while (studentList[pageFloor] && pageFloor < pageCeiling){ //Conditional that makes sure the loop doesn't access
     studentList[pageFloor].style.display = '';               //out of bound array && doesn't overshoot number of entries
     pageFloor++;
@@ -79,10 +44,11 @@ const showPage = (list, page) => {
 
 }
 
-const pagination = () => {
+const appendPageLinks = () => {
   const pageDiv = document.querySelector('.page');
   const div_pagination = manipulateEl.create('div', 'className', 'pagination');
   const ul_pagination = manipulateEl.create('ul');
+  let currentPage = 0;
 
   manipulateEl.append(ul_pagination, div_pagination);
   manipulateEl.append(div_pagination, pageDiv);
@@ -94,21 +60,17 @@ const pagination = () => {
     a.textContent = i;
     manipulateEl.append(manipulateEl.append(a, li), ul_pagination);
   }
-  document.querySelectorAll('div[class="pagination"] ul li')[0].firstElementChild.className = 'active'; //Turning first button "blue"
+  ul_pagination.children[0].firstElementChild.className = 'active'; //Turning first navigation link 'blue'
+  
+  //Activate a listener for the navigation links
+  ul_pagination.addEventListener('click', (e) => {
+    showPage(studentList, e.target.textContent);
+    e.target.className = 'active';
+    ul_pagination.children[currentPage].firstElementChild.className = ''; //Turning current navigation link 'colorless'
+    currentPage = (e.target.textContent - 1);
+  });
 }
 
-pagination();
+appendPageLinks();
 showPage(studentList, 1);
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
 
